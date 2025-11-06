8f04c5b09128a9172683af2622e48cc0f642e526
Ayushman Gupta <guptaayushman24@gmail.com>
2025-11-06 22:49:50 +0530

    New module is created

---
+++ userexpense/src/main/java/com/example/userexpense/UserexpenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.userexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserexpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserexpenseApplication.class, args);
+	}
+
+}
