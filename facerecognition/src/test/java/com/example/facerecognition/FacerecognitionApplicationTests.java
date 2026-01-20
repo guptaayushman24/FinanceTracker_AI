8359b26cf5bf86c85a5ad7495b09f7b82cde332d
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-01-21 00:14:09 +0530

    Embedding Code

---
+++ facerecognition/src/test/java/com/example/facerecognition/FacerecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facerecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacerecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
