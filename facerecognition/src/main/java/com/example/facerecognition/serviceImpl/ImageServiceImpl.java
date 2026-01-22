b8b15d181bbe850df6391084d5d9adc2f1d1ad1b
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-01-23 00:50:21 +0530

    Feign Client

--- facerecognition/src/main/java/com/example/facerecognition/serviceImpl/ImageServiceImpl.java
+++ facerecognition/src/main/java/com/example/facerecognition/serviceImpl/ImageServiceImpl.java
@@ -29,36 +29,6 @@ public class ImageServiceImpl implements ImageService {
         // Read the file into a BufferedImage
         BufferedImage bufferedImage = ImageIO.read(image.getInputStream());
 
-//        // Convert BufferedImage to a consistent type (TYPE_3BYTE_BGR is standard for OpenCV)
-//        BufferedImage imageCopy = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-//        imageCopy.getGraphics().drawImage(bufferedImage, 0, 0, null);
-//
-//        // Get the pixel data
-//        byte[] data = ((DataBufferByte) imageCopy.getRaster().getDataBuffer()).getData();
-//
-//        // Create a Mat and put the data into it
-//
-//        Mat mat = new Mat(imageCopy.getHeight(), imageCopy.getWidth(), (int) CvType.CV_8UC3);
-//
-//        // Preprocess (Resize & Normalize) - example for 224x224
-//        Mat blob = Dnn.blobFromImage(mat,1.0/255.0,new Size(244,244),new Scalar(0,0,0,0),true,false);
-//
-//        // Load Model
-//        Net net = Dnn.readNetFromONNX("D://FinanceTracker_AI//facerecognition//src//main//resources//model//model.onnx");
-//
-//        // Forward Pass
-//        net.setInput(blob);
-//        Mat output = net.forward(); // This mat contains the embedding
-//
-//        // Extract vector (example: flatten the output Mat)
-//        // The output 'Mat' might need reshaping/flattening depending on the model's output layer
-//        float [] embedding = new float[output.cols()];
-//        for (int i=0;i<output.cols();i++){
-//            embedding[i] = (float) output.get(0,i)[0]; // Get values for each dimension
-//        }
-//
-//        return embedding;
-
         BufferedImage imageCopy = new BufferedImage(
                 bufferedImage.getWidth(),
                 bufferedImage.getHeight(),
@@ -77,7 +47,11 @@ public class ImageServiceImpl implements ImageService {
                 false,
                 false);
 
-        Net net = Dnn.readNetFromONNX("D://FinanceTracker_AI//facerecognition//src//main//resources//model//model.onnx");
+        // For Windows
+       //  Net net = Dnn.readNetFromONNX("D://FinanceTracker_AI//facerecognition//src//main//resources//model//model.onnx");
+
+        // For Mac
+        Net net = Dnn.readNetFromONNX("/Users/ayushmangupta/Documents/FinanceTracker_AI/facerecognition/src/main/resources/model/model.onnx");
 
         net.setInput(blob);
         Mat output = net.forward();
